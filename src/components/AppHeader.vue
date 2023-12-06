c9469f0f43abaa6c26bec5e45f50fd57e4952ca0
Francesco Rapetti <rapettif80@yahoo.it>
2023-12-06 18:16:14 +0100

    Bugfix: selected page now stays colored

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -13,11 +13,108 @@ export default {
     },
 
     methods: {
+        // Sets the value of the search property in the store to the input value
         setValue() {
             this.store.search = this.inputValue
+
+            // Calls the buttonFunction
             this.buttonFunction()
+
+            // Resets the input value to an empty string
             document.querySelector('input').value = ''
+        },
+
+        /**
+         * Sets the current page based on the given page parameter.
+         * @param {string} page - The name of the page to set.
+         */
+        setPage(page) {
+            // Get all the navigation links
+            const links = document.querySelectorAll('.nav-link');
+
+            // Switch statement to handle different page cases
+            switch (page) {
+                // If the page is 'home'
+                case 'home':
+                    // Set the store properties
+                    this.store.home = true;
+                    this.store.tvSeries = false;
+                    this.store.film = false;
+                    this.store.myList = false;
+                    this.store.search = '';
+
+                    // Set the color of all links to white
+                    links.forEach(element => {
+                        element.style.color = 'white';
+                    });
+
+                    // Set the color of the 'home' link to red
+                    const home = document.querySelector('#home');
+                    home.style.color = 'red';
+                    break;
+
+                // If the page is 'tv'
+                case 'tv':
+                    // Set the store properties
+                    this.store.home = false;
+                    this.store.tvSeries = true;
+                    this.store.film = false;
+                    this.store.myList = false;
+                    this.store.search = '';
+
+                    // Set the color of all links to white
+                    links.forEach(element => {
+                        element.style.color = 'white';
+                    });
+
+                    // Set the color of the 'tv' link to red
+                    const tv = document.querySelector('#tv');
+                    tv.style.color = 'red';
+                    break;
+
+                // If the page is 'movies'
+                case 'movies':
+                    // Set the store properties
+                    this.store.home = false;
+                    this.store.tvSeries = false;
+                    this.store.film = true;
+                    this.store.myList = false;
+                    this.store.search = '';
+
+                    // Set the color of all links to white
+                    links.forEach(element => {
+                        element.style.color = 'white';
+                    });
+
+                    // Set the color of the 'movies' link to red
+                    const movies = document.querySelector('#movies');
+                    movies.style.color = 'red';
+                    break;
+
+                // If the page is 'myList'
+                case 'myList':
+                    // Set the store properties
+                    this.store.home = false;
+                    this.store.tvSeries = false;
+                    this.store.film = false;
+                    this.store.myList = true;
+                    this.store.search = '';
+
+                    // Set the color of all links to white
+                    links.forEach(element => {
+                        element.style.color = 'white';
+                    });
+
+                    // Set the color of the 'myList' link to red
+                    const myList = document.querySelector('#myList');
+                    myList.style.color = 'red';
+                    break;
+            }
         }
+    },
+
+    mounted() {
+        this.setPage('home')
     }
 }
 </script>
@@ -25,12 +122,7 @@ export default {
 <template>
     <nav class="navbar navbar-expand-lg bg-light position-fixed w-100 z-3 bg-black text-white">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#" @click="
-                store.home = true,
-                store.tvSeries = false,
-                store.film = false,
-                store.myList = false,
-                store.search = ''"><img
+            <a class="navbar-brand" href="#" @click="setPage('home')"><img
                     src="https://fontmeme.com/permalink/231206/504e88f1a30f968e13f4cdd854aab685.png" alt="netflix-font"
                     border="0"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -40,36 +132,16 @@ export default {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#" @click="
-                            store.home = true,
-                            store.tvSeries = false,
-                            store.film = false,
-                            store.myList = false,
-                            store.search = ''">Home</a>
+                        <a id="home" class="nav-link" aria-current="page" href="#" @click="setPage('home')">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#" @click="
-                            store.home = false,
-                            store.tvSeries = true,
-                            store.film = false,
-                            store.myList = false,
-                            store.search = ''">Tv</a>
+                        <a id="tv" class="nav-link" href="#" @click="setPage('tv')">Tv</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#" @click="
-                            store.home = false,
-                            store.tvSeries = false,
-                            store.film = true,
-                            store.myList = false,
-                            store.search = ''">Movies</a>
+                        <a id="movies" class="nav-link" href="#" @click="setPage('movies')">Movies</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#" @click="
-                            store.home = false,
-                            store.tvSeries = false,
-                            store.film = false,
-                            store.myList = true,
-                            store.search = ''">My List</a>
+                        <a id="myList" class="nav-link" href="#" @click="setPage('myList')">My List</a>
                     </li>
                 </ul>
                 <div class="d-flex">
