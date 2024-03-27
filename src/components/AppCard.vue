50dbe9b07fa2f20278b9986a94c8eae316ca8443
Francesco-Rapetti <rapettif80@yahoo.it>
2024-03-27 17:09:23 +0100

    Style changes

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -14,18 +14,48 @@ export default {
 </script>
 
 <template>
-    <div @click="$emit('showInfo', movie)">
+    <div id="card-wrapper" class="position-relative"
+        @click="console.log($event.target.id), $event.target.id == 'backdrop' ? $emit('showInfo', movie) : ''">
+        <button class="position-absolute end-0"><i class="fa-regular fa-heart"></i></button>
         <div class="backdrop-container rounded-3 overflow-hidden">
-            <img class="w-100" :src="store.apiImg + movie.backdrop_path" :alt="movie.title ? movie.title : movie.name">
+            <img id="backdrop" class="w-100" :src="store.apiImg + movie.backdrop_path"
+                :alt="movie.title ? movie.title : movie.name">
         </div>
         <div class="title fs-5">{{ movie.title ? movie.title : movie.name }}</div>
     </div>
 </template>
 
 <style scoped>
+#card-wrapper:hover button {
+    opacity: 1;
+}
+
+button {
+    background-color: transparent;
+    border: none;
+    color: white;
+    font-size: 24px;
+    padding: 10px;
+    cursor: pointer;
+    transition: all .3s ease-in-out;
+    opacity: 0;
+    bottom: 30px;
+    filter: brightness(1.5) !important;
+    z-index: 99999;
+}
+
+button:hover~.backdrop-container {
+    filter: brightness(.6);
+}
+
 .backdrop-container {
     width: 400px;
     cursor: pointer;
+    transition: all .3s ease-in-out;
+}
+
+.backdrop-container:hover {
+    filter: brightness(.6);
 }
 
 .title {
